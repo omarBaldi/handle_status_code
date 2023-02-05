9b0980fed688cce6894e851a0865db37b92a4b53
Omar Baldi <omar.baldi@maersk.com>
2023-02-05 21:22:12 +0100

    Created function to handle api state update

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -2,23 +2,40 @@ import { useEffect, useState } from 'react';
 
 interface ApiResponse<T> {
   loading: boolean;
-  errorMsg: string;
-  data: T | T[];
+  errorMsg: string | null;
+  data: T | T[] | null;
 }
+
+const initialApiState: ApiResponse<any> = {
+  loading: true,
+  errorMsg: null,
+  data: null,
+};
 
 /**
  *
  */
 export const useFetch = <T>({ url }: { url: string }): ApiResponse<T> => {
-  const [apiState, _] = useState<ApiResponse<T>>({
-    loading: false,
-    errorMsg: '',
-    data: [],
+  const [apiState, setApiState] = useState<ApiResponse<T>>({
+    ...initialApiState,
   });
+
+  const handleApiStateUpdated = ({
+    key,
+    updatedValue,
+  }: {
+    key: keyof typeof apiState;
+    updatedValue: typeof apiState[keyof typeof apiState];
+  }): void => {
+    setApiState((prevApiState) => ({ ...prevApiState, [key]: updatedValue }));
+  };
 
   /**
    * Whenever the url changes, I need to perform
    * an async API request.
+   *
+   * TODO: add abort controller to stop previous request made when url changes
+   * TODO: create utility function to return custom error messages based on status code received
    */
   useEffect(() => {
     fetch(url)
@@ -32,8 +49,19 @@ export const useFetch = <T>({ url }: { url: string }): ApiResponse<T> => {
 
         return response.json();
       })
-      .then((data: T) => console.log('Data: ', data))
-      .catch((err) => console.error('Error: ', err));
+      .then((data: T) => handleApiStateUpdated({ key: 'data', updatedValue: data }))
+      .catch((err) =>
+        handleApiStateUpdated({
+          key: 'errorMsg',
+          updatedValue: (err as any)?.message ?? 'Error!',
+        })
+      )
+      .finally(() => handleApiStateUpdated({ key: 'loading', updatedValue: false }));
+
+    //* cleanup on component unmount
+    return () => {
+      setApiState(initialApiState);
+    };
   }, [url]);
 
   return {
